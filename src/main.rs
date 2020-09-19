6d45784d447332344ff07a1e206d91e190906043
Glen Sargent <glensarg@hotmail.co.uk>
2020-09-19 18:48:55 +0100

    paste from private repo

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use std::env;
+use std::process;
+
+mod lib;
+use lib::Config;
+
+fn main() {
+    // make the program take 2 inputs, a string to search for and a file to look in
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = lib::run(config) {
+        eprintln!("application error: {}", e);
+        process::exit(1);
+    }
+
+}
